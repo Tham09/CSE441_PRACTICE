5b48917e9241c4b3824f568d89690ed68233d2d0
Tham09 <bi09louhoang1994@gmail.com>
2024-10-10 05:39:20 +0700

    second commit

--- app/src/main/java/com/example/prac04/MainActivity.java
+++ app/src/main/java/com/example/prac04/MainActivity.java
@@ -77,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu); // Inflate menu
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
